34084687f35cdd32faa0daa60fc0f97dcbfb103c
misheck githongo <misheckgithongo@gmail.com>
2021-08-30 21:51:37 +0300

    using httpModule to get tasks from json server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import  { Task } from '../Task'
 import { TASKS } from '../mock-tasks'
 
@@ -8,10 +9,11 @@ import { TASKS } from '../mock-tasks'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient ) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
